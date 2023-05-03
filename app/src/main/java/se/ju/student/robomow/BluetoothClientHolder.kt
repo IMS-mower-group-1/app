d86a145dddc77b606ff3465813717927c7ffb701
philip <philleabraham@hotmail.com>
2023-05-03 21:31:14 +0200

    Removed unused functions in singleton

--- app/src/main/java/se/ju/student/robomow/BluetoothClientHolder.kt
+++ app/src/main/java/se/ju/student/robomow/BluetoothClientHolder.kt
@@ -2,13 +2,4 @@ package se.ju.student.robomow
 
 object BluetoothClientHolder {
     var bluetoothClient: BluetoothClient? = null
-
-    fun isConnected(): Boolean {
-        return bluetoothClient?.isConnected() ?: false
-    }
-
-    fun disconnect() {
-        bluetoothClient?.disconnect()
-        bluetoothClient = null
-    }
 }
